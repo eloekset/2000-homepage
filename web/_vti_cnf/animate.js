7c04c325fdf092291abdd61a25de1429191c9ea4
Eivind Gussiås Løkseth <eloekset@msn.com>
2022-08-11 00:37:35 +0200

    Add homepage from 2000

---
+++ web/_vti_cnf/animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Eivind
+vti_modifiedby:SR|Eivind
+vti_timecreated:TR|17 Jan 2000 16:10:53 -0000
+vti_timelastmodified:TR|17 Jan 2000 16:10:53 -0000
+vti_backlinkinfo:VX|meny_2.htm
+vti_extenderversion:SR|4.0.2.2717
+vti_syncofs_ftp.sol.no\:21/~loekseth/www_docs:TW|17 Jan 1999 19:56:00 -0000
+vti_syncwith_ftp.sol.no\:21/~loekseth/www_docs:TR|17 Jan 2000 16:10:54 -0000
